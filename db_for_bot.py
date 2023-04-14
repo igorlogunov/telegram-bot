85d0d664753418b453063abb5eff5363c14a7837
Igor Logunov <my-mail-tyt@yandex.ru>
2023-04-14 21:04:45 +0300

    first code-rewiew

--- db_for_bot.py
+++ db_for_bot.py
@@ -30,14 +30,4 @@ class DateBase():
             rows = rows[-10:]
         return rows
 
-# cur.execute('CREATE TABLE requests (id INTEGER AUTOINCREMENT, user, request)')
-#
-# def sql_add():
-#     if message.text == '/low':
-#         cur.execute("INSERT INTO db_bot VALUES (message.from_user.id, 'Запрос маленьких животных')")
-#     if message.text == '/high':
-#         cur.execute("INSERT INTO db_bot VALUES (message.from_user.id, 'Запрос больших животных')")
-#     if message.text == '/custom':
-#         cur.execute("INSERT INTO db_bot VALUES (message.from_user.id, 'Запрос животных по Вашим параметрам')")
-
 my_db = DateBase()
